0c7ed3d789ca973f2a251e7a257e0648d5c4b966
Dawid Czartoszewski <dawid.czartoszewski@gmail.com>
2018-07-12 14:50:07 +0200

    working blog code

--- src/main/java/pl/akademiakodu/springExample/BlogController.java
+++ src/main/java/pl/akademiakodu/springExample/BlogController.java
@@ -1,12 +1,25 @@
 package pl.akademiakodu.springExample;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
     @GetMapping("/")
     public String add(){
         return "add";
+    }
+
+    @GetMapping("/show")
+    public String show(@RequestParam String tittle,
+                       @RequestParam String content,
+                       @RequestParam String author,
+                       ModelMap modelMap)
+    {
+        Post post = new Post(tittle, content, author);
+        modelMap.put("post", post);
+        return "show";
     }
 }
